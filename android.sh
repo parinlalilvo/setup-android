a45b76393ef285c81bcace8c2401d056ac1bf593
pingprart <pingprart@gmail.com>
2021-01-29 10:53:21 +0700

    Update shell script android

--- android.sh
+++ android.sh
@@ -2,15 +2,17 @@
 
 name="$USER"
 
-export ANDROID_HOME=/Users/$name/Library/Android/sdk/
+export ANDROID_HOME=/Users/$name/Library/Android/sdk
 export PATH=${ANDROID_HOME}:$PATH
 
-export ANDROID_TOOLS=${ANDROID_HOME}/tools/
-export PATH=${ANDROID_TOOLS}bin/:$PATH
+export ANDROID_TOOLS=${ANDROID_HOME}/tools
+export PATH=${ANDROID_TOOLS}:$PATH
 
-export ANDROID_PLATFORM_TOOLS=${ANDROID_HOME}/platform-tools/
+export ANDROID_TOOLS_BIN=${ANDROID_HOME}/tools/bin
+export PATH=${ANDROID_TOOLS_BIN}/:$PATH
+
+export ANDROID_PLATFORM_TOOLS=${ANDROID_HOME}/platform-tools
 export PATH=${ANDROID_PLATFORM_TOOLS}:$PATH
-
 
 # Android
 alias deeplink="adb shell am start -a android.intent.action.VIEW -d $1"
